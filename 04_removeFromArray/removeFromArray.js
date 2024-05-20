d2f5ea45b595145197eeea5fb2f592d8b94b09ce
Discusser <47938380+Discusser@users.noreply.github.com>
2024-05-20 15:06:54 +0200

    Complete exercises 04, 05, 06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  const elements = Array.from(arguments).slice(1);
+  const newArray = [];
 
+  for (let i = 0; i < array.length; i++) {
+    if (!elements.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (typeof start !== "number" || typeof end !== "number" || start < 0 || end < 0) {
+    return "ERROR";
+  }
 
+  return (start + end - 1) * (start + end) / 2;
 };
 
 // Do not edit below this line
