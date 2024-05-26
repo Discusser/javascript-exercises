ee57e613d8b7ed66d71d20edbaa3fc6d8e28f0e1
Discusser <47938380+Discusser@users.noreply.github.com>
2024-05-26 22:07:58 +0200

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    string = string.toLowerCase().replaceAll(/[,.!? ]/g, "");
 
+    return string.slice(0, Math.ceil(string.length / 2)).split("").reverse().join("") === string.slice(Math.floor(string.length / 2));
 };
 
 // Do not edit below this line
