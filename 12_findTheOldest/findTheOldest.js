5ec730011a1ac1c8467aac3bbdfa5fe24ffb349a
Discusser <47938380+Discusser@users.noreply.github.com>
2024-05-26 22:18:56 +0200

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(person) {
+        const endYear = person.yearOfDeath || new Date().getFullYear();
+        const startYear = person.yearOfBirth;
 
+        return endYear - startYear;
+    }
+
+    return people.sort((current, next) => getAge(current) - getAge(next)).at(-1);
 };
 
 // Do not edit below this line
